e4bda87879e918e0ef015a9b857afb40218630af
Tanvi Joshi <joshitanvi9905@gmail.com>
2026-03-30 22:44:20 +0530

    Added turtle examples

---
+++ Turtle/plus.py
@@ -0,0 +1,11 @@
+import turtle
+t = turtle.Turtle()
+for i in range(4):
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.right(90)
+turtle.done()    
+    
---
+++ Turtle/squareinsquare.py
@@ -0,0 +1,9 @@
+import turtle as t
+
+j=120
+for _ in range(5):
+    for _ in range(4):
+     t.forward(j)
+     t.left(90)
+    j+=30
+t.done()
